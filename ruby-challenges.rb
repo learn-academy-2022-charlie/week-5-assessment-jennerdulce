67ac5b6a683cac41d80f36cce55bc997fcd31917
jennerdulce <jennerdulce@gmail.com>
2022-06-12 11:09:03 -0700

    Finished assessment

--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -10,6 +10,14 @@ letter_o = 'o'
 letter_t = 't'
 # Expected output: ['tea', 'water', 'soda water']
 
+def get_word_including_letter(arr, letter)
+    arr.select do |value|
+        value.include?(letter)
+    end
+end
+p get_word_including_letter(beverages_array, letter_o)
+p get_word_including_letter(beverages_array, letter_t)
+
 
 # -------------------2) Create a method that takes in an array of numbers and returns the sum of the numbers. Use the test variables provided.
 
@@ -19,13 +27,19 @@ nums_array1 = [42, 7, 27]
 nums_array2 = [25, 17, 47, 11]
 # Expected output: 100
 
-
+def add_all_numbers arr
+    total_sum = 0
+    arr.each do |value|
+        total_sum = total_sum + value
+    end
+    total_sum
+end
+p add_all_numbers(nums_array1)
+p add_all_numbers(nums_array2)
 
 # --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
 
 # Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
-
-
 
 # -------------------3b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed by a given amount. The brake method should decrease the speed by a given amount. The bike cannot go negative speeds.
 
@@ -33,3 +47,38 @@ nums_array2 = [25, 17, 47, 11]
 # Expected output example: my_bike.pedal_faster(18) => 28
 # Expected output example: my_bike.brake(5) => 23
 # Expected output example: my_bike.brake(25) => 0
+
+class Bike
+    attr_accessor :model, :wheels, :current_speed
+    def initialize(model, wheels, current_speed=0)
+        @model = model
+        @wheels = wheels
+        @current_speed = current_speed
+    end
+
+    def bike_info
+        "The #{model} bike has #{wheels} wheels and is going #{current_speed} mph."
+    end
+
+    def pedal_faster(speed)
+        @current_speed = current_speed + speed
+    end
+
+    def break(speed)
+        if @current_speed - speed < 0
+            @current_speed = 0
+        else
+            @current_Speed = @current_speed - speed
+        end
+    end
+end
+
+fixie_bicycle = Bike.new('Fixie', 2)
+p fixie_bicycle.model
+p fixie_bicycle.wheels
+p fixie_bicycle.current_speed
+p fixie_bicycle.bike_info
+p fixie_bicycle.pedal_faster(20)
+p fixie_bicycle.break(15)
+p fixie_bicycle.break(100)
+p fixie_bicycle.bike_info
